513f5c0b54cd9e0117a1058078c413a3a9922f91
Vlad Doster <mvdoster@gmail.com>
2020-07-12 00:53:52 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
-var webpack = require('webpack')
+var webpack = require('webpack')	
 const path = require('path')
+
 module.exports = {
   entry: {
-    'files-vdoster.js': [
-      path.resolve(__dirname, 'files-vdoster.js')
-    ]
+    'rap-diablo.vdoster.com': './rap-diablo.vdoster.com/main.js',
+    'files.vdoster.com': './files.vdoster.com/main.js'
   },
   output: {
-    filename: 'files-vdoster.js',
-    path: path.resolve(__dirname, '../public')
+    path: __dirname,
+    filename: "sites/[name]/build/bundle.js"
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ['rap-diablo.vdoster.com'],
+      filename: 'subdomains/rap-diablo.vdoster.com/build/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ['files.vdoster.com'],
+      filename: 'subdomains/files.vdoster.com/build/index.html'
     })
   ]
-}
+};
