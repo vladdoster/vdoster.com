2c4f80560278d7a06a25136452f4903ac52dd36b
Vlad Doster <mvdoster@gmail.com>
2021-01-03 04:22:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ var Commands = function(hostline, user, group) {
          softLink(user, group, 'linkedin',   'http://linkedin.vdoster.com') + '\n' +
          softLink(user, group, 'music',      'http://rap-diablo.vdoster.com') + '\n' +
          file(    user, group, 'keybase.txt','https://raw.githubusercontent.com/vladdoster/vdoster.com/master/keybase.txt') + '\n' +
-         file(    user, group, 'resume.pdf', 'https://drive.google.com/file/d/1jeVb7rZ6IN4288ayXhmJ6vU44O4rFddC/view?usp=drivesdk)
+         file(    user, group, 'resume.pdf', 'https://drive.google.com/file/d/1jeVb7rZ6IN4288ayXhmJ6vU44O4rFddC/view?usp=drivesdk')
          // 'https://github.com/vladdoster/resume-src-code/raw/master/resume.pdf')
 
     },
